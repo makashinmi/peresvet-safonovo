3da56be3d5836ef0e414f79bb82f041745852a58
postmodern1st <mixanidze3521@gmail.com>
2023-05-22 04:12:23 +0300

    work on backend is done

--- backend/index/views.py
+++ backend/index/views.py
@@ -5,13 +5,12 @@ from .models import *
 
 # Create your views here.
 def index(request: rq):
-    
-
     context = {
-        'slider_list': [],
+        'slider_list': [(index+1, photo) for index, photo in enumerate(Slider_Photo.objects.all()[:10])], 
         'trainer_list': Trainer.objects.all(),
         'post_list': Post.objects.all()[:3],
         'photo_list': Photo.objects.all()[:12],
     }
-
+    
+    
     return render(request, 'index.html', context)
